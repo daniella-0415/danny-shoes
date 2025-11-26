aa47331ec1877498339563ed6bb6bec592a24929
Daniella <judithdaniella6@gmail.com>
2025-11-26 10:39:05 +0200

    Initial frontend commit

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -21,7 +21,7 @@ export default function Signin() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/signin", {
+      const res = await fetch("http://16.171.156.22:3000/api/users/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -32,7 +32,7 @@ export default function Signup() {
       console.log("Sending payload:", payload);
 
       // Use direct backend URL
-      const response = await fetch("http://localhost:3000/signup", {
+      const response = await fetch("http://16.171.156.22:3000/api/users/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
